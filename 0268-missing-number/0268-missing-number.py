26ee409af76a128275c6e9698cf1054849aa7117
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-03-16 17:45:04 +0100

    Time: 3 ms (64.43%), Space: 18.7 MB (73.11%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range (0,len(nums)):
-            if i!=nums[i]:
-                return i
-        return len(nums)
-        
+        xor=len(nums)
+        for i in range(len(nums)):
+            xor=xor^i^nums[i]
+        return xor
