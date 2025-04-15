d6159ef6fa5131b434cf1cdcc6a019de275bd4a9
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-04-15 14:09:29 +0200

    Time: 40 ms (51.42%), Space: 17.7 MB (82.1%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,7 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         sum=0
-        for i in range(32):
-            bit=(n>>i) & 1
-            sum =sum| bit<<(31-i)
+        for i in range(31,-1,-1):
+            c=n%2
+            sum=sum+((2**i)*c)
+            n=n//2
         return sum
+        
