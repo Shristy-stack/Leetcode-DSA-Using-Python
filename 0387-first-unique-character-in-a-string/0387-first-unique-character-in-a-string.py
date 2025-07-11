3383e7a090f31f3c67c6382b87b3ea2964660962
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-07-11 11:22:53 +0200

    Time: 55 ms (66.75%), Space: 18.1 MB (55.83%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,19 +1,12 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        hashmap = {}
-        for i in s:
-            if i in hashmap:
-                hashmap[i] += 1
-            else:
-                hashmap[i] = 1
-
-        c = 0
-        for i in hashmap:
-            if hashmap[i] == 1:
-                c = i
-                break
-
+        hashmap={}
         for i in range(len(s)):
-            if s[i] == c:
+            if s[i] in hashmap:
+                hashmap[s[i]]=-1
+            else:
+                hashmap[s[i]]=i
+        for i in hashmap.values():
+            if i!=-1:
                 return i
         return -1
