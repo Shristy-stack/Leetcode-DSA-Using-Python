25a4b88804c1fc765e0e5d86ce50cbf3ef901d24
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-07-12 23:00:12 +0200

    Time: 0 ms (100%), Space: 17.9 MB (65.34%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        hashmap={}
+        for i in range(len(arr)):
+            hashmap[arr[i]]=1+hashmap.get(arr[i],0)
+        return len(set(hashmap.values()))==len(hashmap.values())
+        
