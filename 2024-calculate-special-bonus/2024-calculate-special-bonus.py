ac11539cb63e6372f80d344b827992378dd24707
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-09-01 17:48:36 +0200

    Time: 361 ms (17.75%), Space: 67.5 MB (87.74%) - LeetHub

--- 2024-calculate-special-bonus/2024-calculate-special-bonus.py
+++ 2024-calculate-special-bonus/2024-calculate-special-bonus.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    condition=employees['bonus']=(employees['employee_id']%2!=0) & (~employees['name'].str.startswith("M"))
-    employees['bonus']=0
-    employees.loc[condition,'bonus']=employees['salary']
+    cond=(employees["employee_id"]%2==1)&(~employees["name"].str.startswith("M"))
+    employees["bonus"]=employees["salary"].where(cond,0)
     return employees[['employee_id','bonus']].sort_values('employee_id')
-    
