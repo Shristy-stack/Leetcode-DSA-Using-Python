d9f861e5d293972ee15c2c402996c7eed01717f3
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-09-17 11:30:49 +0200

    Time: 20 ms (6.3%), Space: 18 MB (50.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dict1={}
-        dict2={}
-        for i in s:
-            if i in dict1:
-                dict1[i]+=1
-            else:
-                dict1[i]=1
-        for i in t:
-            if i in dict2:
-                dict2[i]+=1
-            else:
-                dict2[i]=1
-        return dict1==dict2
+        if len(s)!=len(t):
+            return False
+        hashmap={}
+        for i in range(len(s)):
+            hashmap[s[i]]=hashmap.get(s[i],0)+1
+        for j in range(len(t)):
+            if t[j] not in hashmap or hashmap[t[j]]==0:
+                return False
+            hashmap[t[j]]-=1
+        return True
