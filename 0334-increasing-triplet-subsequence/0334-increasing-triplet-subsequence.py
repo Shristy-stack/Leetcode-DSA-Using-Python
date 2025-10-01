3747001c1efea81399094019b270db73c4360af4
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-10-01 14:31:40 +0200

    Time: 33 ms (23.77%), Space: 37.5 MB (22.47%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first,second=float('inf'),float('inf')
+        for i in range(len(nums)):
+            if nums[i]<=first :
+                first=nums[i]
+            elif nums[i]<=second:
+                second=nums[i]
+            else:
+                return True
+        return False
