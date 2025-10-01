31b2a71307698fbde3f00e154172151dcca2f844
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-10-01 14:37:19 +0200

    Time: 4 ms (60.22%), Space: 18.8 MB (49.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,8 +3,11 @@ class Solution:
         k=0
         for i in range(len(nums)):
             if nums[i]!=0:
-                nums[k],nums[i]=nums[i],nums[k]
+                nums[k]=nums[i]
                 k+=1
+        for i in range(k,len(nums)):
+            nums[i]=0
+        return nums
 
 
 
