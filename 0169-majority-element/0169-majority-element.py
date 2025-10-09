a62c252368a7a6a29eff7196167a3cb379eda2db
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-10-09 11:26:00 +0200

    Time: 11 ms (28.27%), Space: 19.3 MB (50.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,10 +3,4 @@ class Solution:
         hashmap={}
         for num in nums:
             hashmap[num]=hashmap.get(num,0)+1
-        maxp=float('-inf')
-        res=0
-        for key,value in hashmap.items():
-            if value > maxp:
-                maxp=value
-                res=key
-        return res
+        return max(hashmap, key=hashmap.get)
