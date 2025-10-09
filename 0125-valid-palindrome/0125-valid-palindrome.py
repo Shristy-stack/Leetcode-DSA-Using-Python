534b640d1ca10b27a941c972846e07aff2a6fdf9
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-10-09 08:21:28 +0200

    Time: 7 ms (82.16%), Space: 19.4 MB (22.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,6 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s=s.lower().replace(" ","")
-        cleaned = re.sub(r'[^A-Za-z0-9]', '', s)
-        cleanedp=cleaned[::-1]
-        return cleaned==cleanedp
+       s=s.lower().replace(" ","")
+       s = re.sub(r"[^A-Za-z0-9]", "", s)
+       return  s==s[::-1]
