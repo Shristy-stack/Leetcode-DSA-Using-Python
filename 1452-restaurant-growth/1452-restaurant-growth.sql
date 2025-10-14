e3e6e802114877e3f6cb36aeff1278a3b463c832
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-10-14 18:22:34 +0200

    Time: 355 ms (44.54%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -3,9 +3,9 @@ WITH sum_as as
 (
     SELECT visited_on , SUM(amount) as amount 
     FROM Customer GROUP BY visited_on
-)
-
-SELECT
+),
+sump as
+(SELECT
   visited_on,
   SUM(amount) OVER (
     ORDER BY visited_on
@@ -15,6 +15,8 @@ SELECT
     ORDER BY visited_on
     ROWS BETWEEN 6  PRECEDING AND CURRENT ROW
   ),2) AS average_amount 
-FROM sum_as
-order by visited_on
-LIMIT 18446744073709551615 OFFSET 6
+  , ROW_NUMBER() OVER (ORDER BY visited_on ASC) AS rn
+FROM sum_as)
+
+SELECT visited_on,amount,average_amount FROM sump
+WHERE rn>=7
