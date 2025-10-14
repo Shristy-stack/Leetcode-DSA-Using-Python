960b9051c509480e082ca5aaa925506504a2ddb5
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-10-14 18:54:57 +0200

    Time: 364 ms (14.71%), Space: 17.8 MB (23.5%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -4,11 +4,4 @@ class Solution:
             return 0
         if n==1:
             return 1
-        a,b=0,1
-        c=0
-        while n>=2:
-            c=a+b
-            a=b
-            b=c
-            n-=1
-        return c
+        return self.fib(n-1)+self.fib(n-2)
