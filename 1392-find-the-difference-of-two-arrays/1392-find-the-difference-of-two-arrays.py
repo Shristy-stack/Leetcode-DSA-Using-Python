d1de0c475958f20315fa389d3e0fa85435db9cde
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-10-21 18:21:10 +0200

    Time: 8 ms (49.01%), Space: 18 MB (66.05%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.py
@@ -1,14 +1,4 @@
-from collections import Counter
+
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        hashmap1,hashmap2=Counter(set(nums2)), Counter(set(nums1))
-        res1,res2=set(),set()
-        for num in nums1:
-            if num not in hashmap1 :
-                res1.add(num)
-        for num in nums2:
-            if num not in hashmap2 :
-                res2.add(num)
-        return [list(res1),list(res2)]
-
-
+        return [list(set(nums1)-set(nums2)),list(set(nums2)-set(nums1))]
