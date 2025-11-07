8f16ac94636eff4cbed3559a4972fd5a4c06fefa
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-11-07 05:49:35 +0100

    Time: 246 ms (93.88%), Space: 68.2 MB (57.38%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.py
+++ 0178-rank-scores/0178-rank-scores.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
     scores['rank']=scores['score'].rank(method='dense',ascending=False)
-    return scores[['score','rank']].sort_values(by='rank',ascending=True)
+    return scores[['score','rank']].sort_values(by='rank')
