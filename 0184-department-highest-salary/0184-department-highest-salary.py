a3d3a823b2dff51050cadb3af07554d4243943da
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-11-07 05:21:24 +0100

    Time: 306 ms (71.23%), Space: 68.4 MB (94.96%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-    df=pd.merge(employee,department,left_on='departmentId',right_on='id')
-    dff=df.drop(columns=['id_y']).rename(columns={'id_x':'emp_id','name_x':'Employee','salary':'Salary','name_y':'Department'})
-    dff['max_salary']=dff.groupby(['departmentId','Department'])['Salary'].transform('max')
-    res=dff.loc[dff['Salary']==dff['max_salary']].drop(columns=['max_salary'])
-    return res[['Department','Employee','Salary']]
+    df=pd.merge(employee,department, left_on='departmentId',right_on='id')
+    df=df.drop(columns={'id_x','id_y'}).rename(columns={'name_x':'Employee','name_y':'Department','salary':'Salary'})
+    df['max_salary']=df.groupby('Department')['Salary'].transform('max')
+    df=df.loc[df['Salary']==df['max_salary']]
+    return df[['Department','Employee','Salary']]
