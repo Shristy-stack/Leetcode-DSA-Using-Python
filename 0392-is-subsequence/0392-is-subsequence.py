f1234b347cc2c277ccbd1a04840b34175d3df66a
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2025-11-19 10:38:15 +0100

    Time: 0 ms (100%), Space: 18 MB (24.71%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,14 +1,17 @@
 from collections import Counter
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s)>len(t):
+            return False
+        if len(s)==0:
+            return True
         i,j=0,0
-        while i<len(s):
-            if j==len(t):
-                return False
-            if s[i]==t[j]:
-                #print(t[j])
+        count=0
+        while j<len(t):
+            if s[i]==t[j] and i==count:
                 i+=1
-                
+                count+=1
+            if i==len(s):
+                return True
             j+=1
-            #print(j)
-        return True
+        return False
