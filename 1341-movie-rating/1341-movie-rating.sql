87c754fc8b00f44254c7fd2430194e5599cc7a08
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2026-01-01 11:06:14 +0100

    Time: 1044 ms (98.64%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,27 +1,18 @@
-# Write your MySQL query statement below
-WITH cs as (SELECT mr.user_id,mr.movie_id,u.name,m.title,mr.rating , mr.created_at  
-FROM MovieRating as mr JOIN Movies as m
-ON mr.movie_id = m.movie_id
-JOIN Users as u        
-ON mr.user_id = u.user_id) 
-,avg_ratings AS (
-    SELECT 
-        movie_id,
-        title,
-        AVG(rating) AS avg_rating
-    FROM cs
-    WHERE created_at LIKE '2020-02%'
-    GROUP BY movie_id, title
-),
-max_rating AS (
-    SELECT MAX(avg_rating) AS max_avg
-    FROM avg_ratings
+With js as (SELECT m.movie_id ,m.title , u.user_id, u.name , mr.rating, mr.created_at 
+FROM MovieRating mr  JOIN Movies m
+ON m.movie_id=mr.movie_id 
+JOIN Users u
+ON u.user_id=mr.user_id )    
+
+  (SELECT name AS results FROM js 
+GROUP BY user_id
+ORDER BY count(*) desc, name asc LIMIT 1 )
+UNION ALL 
+(
+    SELECT title AS results FROM js 
+    WHERE created_at >='2020-02-01' AND created_at <='2020-02-29'
+    GROUP BY movie_id
+    ORDER BY AVG(rating) DESC, title asc LIMIT 1
+
 )
-(SELECT * FROM (SELECT name as results FROM cs
-GROUP BY user_id 
-ORDER BY COUNT(user_id) DESC ,name ASC LIMIT 1) AS t1)
-UNION ALL
-(SELECT * FROM (SELECT title AS results
-FROM avg_ratings
-WHERE avg_rating = (SELECT max_avg FROM max_rating)
-ORDER BY title limit 1 ) AS t2)
+
