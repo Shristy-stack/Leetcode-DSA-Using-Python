cc7dca47da66546cea36ff9b5399801d7110633b
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2026-01-01 14:25:53 +0100

    Time: 292 ms (26.31%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,3 +1,3 @@
-SELECT x, y, z,
-(CASE WHEN x+y>z AND y+z>x AND x+z>y THEN 'Yes' ELSE 'No' END) AS triangle
-FROM Triangle 
+SELECT x, y, z , 
+(CASE WHEN x+y>z AND y+z>x AND z+x>y THEN "Yes" ELSE "No" END) AS triangle 
+FROM Triangle
