c91f59294fe15ab4e524d5fd6f7f6bc4a83eb067
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2026-02-02 09:33:02 +0100

    Time: 1538 ms (48.68%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,11 +1,13 @@
-SELECT 'Low Salary' AS category, 
-SUM(CASE WHEN income<20000 THEN 1 ELSE 0 END) AS accounts_count 
+(SELECT "Low Salary" AS category , COUNT(*) AS accounts_count 
 FROM Accounts
-UNION ALL 
-SELECT 'Average Salary' AS category, 
-SUM(CASE WHEN income>=20000 AND income<=50000 THEN 1 ELSE 0 END) AS accounts_count 
-FROM Accounts
+WHERE income < 20000)
 UNION ALL
-SELECT 'High Salary' AS category, 
-SUM(CASE WHEN income>50000 THEN 1 ELSE 0 END) AS accounts_count 
+( SELECT "Average Salary" AS category , COUNT(*) AS accounts_count 
 FROM Accounts
+WHERE income >= 20000 AND income <= 50000
+)
+UNION ALL
+( SELECT "High Salary" AS category , COUNT(*) AS accounts_count 
+FROM Accounts
+WHERE income > 50000
+)
