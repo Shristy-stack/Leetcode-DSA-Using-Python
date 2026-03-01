cb15dad78aeaf386ee39f4d586c03c3701488939
SHRISTY SARKAR <sarkar05shristy@gmail.com>
2026-03-01 13:00:55 +0100

    Time: 313 ms (56.73%), Space: 69 MB (39.37%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-    df=pd.merge(employee,department, left_on='departmentId',right_on='id')
-    df=df.drop(columns={'id_x','id_y'}).rename(columns={'name_x':'Employee','name_y':'Department','salary':'Salary'})
-    df['max_salary']=df.groupby('Department')['Salary'].transform('max')
-    df=df.loc[df['Salary']==df['max_salary']]
-    return df[['Department','Employee','Salary']]
+    df=pd.merge(employee,department,left_on='departmentId',right_on='id')
+    df['Salary']=df.groupby('departmentId')['salary'].transform('max')
+    p=df[df['salary']==df['Salary']]
+    return p[['name_y','name_x','Salary']].rename(columns={'name_y':'Department','name_x':'Employee'})
